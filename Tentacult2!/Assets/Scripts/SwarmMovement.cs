8d6a9e285a5ae390b15a7b82b7d7e3444df0e948
Lafsnb <ikingst7@gmail.com>
2020-03-24 09:32:54 -0600

    Swarm

---
+++ Tentacult2!/Assets/Scripts/SwarmMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwarmMovement : MonoBehaviour
+{
+
+    public Transform target;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        //target = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target);
+    }
+}
