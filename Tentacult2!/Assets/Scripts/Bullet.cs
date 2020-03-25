b74aa2efb17c7b166b2f45c78eaeda70751d9c71
Lafsnb <ikingst7@gmail.com>
2020-03-24 22:37:18 -0600

    push

---
+++ Tentacult2!/Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float force = 90;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * force);
+        Destroy(gameObject, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Tentacult2!/Assets/Scripts/SwarmMovement.cs
+++ Tentacult2!/Assets/Scripts/SwarmMovement.cs
@@ -5,21 +5,53 @@ using UnityEngine.AI;
 
 public class SwarmMovement : MonoBehaviour
 {
+    public Transform lookAt;
+    public float attackRange;
+    public float attackSpeed;
+    public GameObject bulletPrefab;
 
     Transform target;
     NavMeshAgent agent;
+    Transform bulletEmitter;
+    bool shooting;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         target = GameObject.FindGameObjectWithTag("Target").transform;
+        bulletEmitter = transform; //TODO change this to a gameobject later
+        shooting = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         agent.SetDestination(target.position);
+        if (Vector3.Distance(transform.position, lookAt.position) < attackRange)
+        {
+            gameObject.transform.LookAt(lookAt);
+            if (!shooting)
+            {
+                shooting = true;
+                StartCoroutine("Shoot");
+            }
+                
+        }
+           
+    }
+
+    IEnumerator Shoot()
+    {
+        
+        //TODO fire bullet
+        GameObject bulletHandler;
+        bulletHandler = Instantiate(bulletPrefab, bulletEmitter.position, bulletEmitter.rotation) as GameObject;
+
+        yield return new WaitForSeconds(attackSpeed);
+        //bulletHandler.transform.Rotate(Vector3.left * 90);
+        shooting = false;
+
     }
 
 }
